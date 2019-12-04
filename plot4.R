eb8be05fe6da2cdda9c7d1265feb32415160c868
nefedovigor <nefedovigor@gmail.com>
2019-12-05 00:56:27 +0300

    Comments added

--- plot4.R
+++ plot4.R
@@ -24,18 +24,27 @@ df_uci <- df_uci %>%
 
 ## plot functions
 png(filename = "plot4.png")
+
+## defines margins and plot postioning
 par(mfrow = c(2, 2), mar = c(4, 4, 4, 4), oma = c(1, 1, 1, 1))
+
+## 1st plot
 with(df_uci_1, plot(Global_active_power ~ date_time, type = "l", 
                     ylab = "Global Active Power (kilowatts)", xlab =""))
+
+## 2nd plot
 with(df_uci_1, plot(Voltage ~ date_time, type = "l", 
                     ylab = "Voltage", xlab =""))
+
+## 3rd plot
 with(df_uci_1, plot(Sub_metering_1 ~ date_time, type = "l",
                     ylab = "Energy sub metering", xlab =""))
 lines(df_uci$Sub_metering_2 ~ df_uci$date_time, type = "l", col = "red")
 lines(df_uci$Sub_metering_3 ~ df_uci$date_time, type = "l", col = "blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
        col = c("black", "red", "blue"), lty = 1, cex = 0.6, bty = "n", xjust = 1)
+
+## 4th plot
 with(df_uci_1, plot(Global_reactive_power ~ date_time, type = "l", 
                     ylab = "Voltage", xlab =""))
-
 dev.off()
